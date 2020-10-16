a0cb7b6aaa0a080da7eaa5675367e6797d9f8bfd
lkvreddy007 <lkvreddy007@gmail.com>
2020-10-17 00:52:52 +0530

    [Krishna]added-InvalidInputException

--- main/java/com/capg/HotelReservation.java
+++ main/java/com/capg/HotelReservation.java
@@ -103,7 +103,7 @@ public class HotelReservation {
 			}
 		});
 		list.stream().forEach
-        (k-> System.out.println("Highest Rated Hotel is: \n"+k.getName()+", Total Rates: $"+calcTotal(k)));
+        (k-> System.out.println("Cheapest Best Rated Hotel is: \n"+k.getName()+", Total Rates: $"+calcTotal(k)));
 		
 		return list;
 	}
